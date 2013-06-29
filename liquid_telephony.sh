62c088b017f153499c7ffa9007ae91c459aaca6d
Joe Black <jblack248@gmail.com>
2013-06-29 16:09:04 -0400

    Fix path issue in script
    Change-Id: I0d2f5ff7cec2c3f0e6f01684ccc85bff70b0fd50

--- liquid_telephony.sh
+++ liquid_telephony.sh
@@ -10,7 +10,7 @@ rm system.img
 apktool d system/framework/framework.jar framework.jar.out/
 rm -r framework.jar.out/smali/android/telephony/
 rm -r framework.jar.out/smali/com/android/internal/telephony/
-tar -xvzf ../../../../device/htc/mecha/liquid_telephony_smali.tar.gz -C framework.jar.out/
+tar -xvzf ../../../../vendor/htc/mecha-radio/liquid_telephony_smali.tar.gz -C framework.jar.out/
 apktool b framework.jar.out/
 
 rm system/framework/framework.jar
